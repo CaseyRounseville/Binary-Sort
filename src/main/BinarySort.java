8fe3d1340c165494033e84f7908dad7e6cc6630b
Casey Rounseville <cas2136714@maricopa.edu>
2018-03-25 21:46:34 -0700

    not quite working right

--- src/main/BinarySort.java
+++ src/main/BinarySort.java
@@ -1,7 +1,69 @@
 package main;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class BinarySort {
 	public static void main(String[] args) {
-		System.out.println("BinarySort: main");
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		String[] tokens = input.split("\\s+");
+		
+		int[] arr = new int[tokens.length];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(tokens[i]);
+		}
+		
+		System.out.println("initial array: " + Arrays.toString(arr));
+		binarySort(arr);
+		System.out.println("sorted array: " + Arrays.toString(arr));
+	}
+	
+	public static void binarySort(int[] arr) {
+		System.out.println("beginning binary sort");
+		for (int i = 1; i < arr.length; i++) {
+			int target = arr[i];
+			int comp = arr[i - 1];
+			move(arr, binarySearch(arr, target, 0, i - 1), i);
+			System.out.println(Arrays.toString(arr));
+		}
+		System.out.println("done with binary sort");
+	}
+	
+	public static int binarySearch(int[] arr, int target, int start, int end) {
+		System.out.println("start: " + start + "\tend: " + end + "\ttarget: " + target);
+		
+		if (start <= end) {
+			if (arr[start] < target) {
+				return start + 1;
+			} else {
+				return start;
+			}
+		}
+		
+		int mid = (start + end) / 2;
+		
+		if (arr[mid] == target) {
+			return mid;
+		} else if (arr[mid] > target) {
+			if (mid - 1 == -1) {
+				return 0;
+			} else {
+				return binarySearch(arr, target, start, mid - 1);
+			}
+		} else {
+			return binarySearch(arr, target, mid + 1, end);
+		}
+	}
+	
+	/*
+	assumes dest < src
+	*/
+	public static void move(int[] arr, int dest, int src) {
+		int temp = arr[src];
+		for (int i = src - 1; i > dest - 1; i--) {
+			arr[i + 1] = arr[i];
+		}
+		arr[dest] = temp;
 	}
 }
